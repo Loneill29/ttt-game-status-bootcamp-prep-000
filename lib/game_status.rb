b3e4a9550225fe11cd41ab6c19aee2409d02c049
Linda ONeill <oneill.linda21@gmail.com>
2017-11-29 18:12:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,12 +13,30 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
 def won?(board)
-WIN_COMBINATIONS.each do |index|
-  if WIN_COMBINATIONS == true
-    return index
-  end
-  end
-else false
+  win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+   position_1 = board[win_index_1] # load the value of the board at win_index_1
+ position_2 = board[win_index_2] # load the value of the board at win_index_2
+ position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+ WIN_COMBINATIONS.each do |WIN_COMBINATIONS|
+   if WIN_COMBINATIONS == true
+     return index
+   end
+   end
+ else false
+ end
+ if position_1 == "X" && position_2 == "X" && position_3 == "X"
+   return win_combination # return the win_combination indexes that won.
+ elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+   return win_combination
+ else return
+   false
+ end
 end
+  
+
 # Define your WIN_COMBINATIONS constant
