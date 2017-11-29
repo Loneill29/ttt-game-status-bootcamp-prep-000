f1d3c3dbdeac3b5186540cd453476a6c5ca59fc1
Linda ONeill <oneill.linda21@gmail.com>
2017-11-29 18:31:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,17 @@ def won?(board)
 if position_1 == "O" && position_2 == "O" && position_3 == "O"
    return win_combination
  end
+ return false
  end
+
+
+ WIN_COMBINATIONS.each do |win_combo|
+ +      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+ +        return win_combo
+ +      end
+ +    end
+ +    return false
+ +  end
 
 
 
