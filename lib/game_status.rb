4470b4d2176910199cac744307989d9596c6b9fc
Linda ONeill <oneill.linda21@gmail.com>
2017-11-29 18:25:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,7 @@ def won?(board)
    return win_combination
  end
  end
- elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+if position_1 == "O" && position_2 == "O" && position_3 == "O"
    return win_combination
  end
  end
