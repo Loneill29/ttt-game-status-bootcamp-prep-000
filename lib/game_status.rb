8dba836665485c52e3be3fc5e24f6df9a214dd61
Linda ONeill <oneill.linda21@gmail.com>
2017-11-29 18:31:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,16 +43,3 @@ def won?(board)
  return false
  end
 
-
- WIN_COMBINATIONS.each do |win_combo|
- +      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
- +        return win_combo
- +      end
- +    end
- +    return false
- +  end
-
-
-
-
-# Define your WIN_COMBINATIONS constant
